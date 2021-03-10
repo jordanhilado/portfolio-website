b37e889398bb4b27cb9c5ac555e7857250c030c1
jordanhilado <37820188+jordanhilado@users.noreply.github.com>
2021-03-09 23:56:10 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
               <Typical
               loop={Infinity}
               steps={['Student 📚', 1500,
-                    'Developer 👨‍💻', 1500,
+                    'Developer 👨💻', 1500,
                     'Laker Fan 🏀', 1500,
                     'Open Sourcer 🛠', 1500,
                     'Hydro Homie 🚰', 1500,
@@ -41,7 +41,7 @@ function App() {
         <div className="App">
           {/* <Link to="/" style={{ textDecoration: 'none' }}><h1 className="flex name">Jordan Ali Hilado</h1></Link>
           <Nav /> */}
-          <Switch>
+          <Switch> 
             <Route path="/" exact component={Home} />
             <Route path="/about" component={About} />
             <Route path="/projects" component={Projects} />
