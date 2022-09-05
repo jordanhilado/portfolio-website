53999b1ae0133bce545c3eeac73a54d14e82d0fb
Jordan Hilado <jordanalihilado@gmail.com>
2022-09-05 02:36:59 -0700

    update app

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,33 +23,21 @@ const Home: NextPage = () => {
         </p>
 
         <div className={styles.grid}>
-          {/* <a href="https://nextjs.org/docs" className={styles.card}>
+          <a href="/education" className={styles.card}>
             <h2>education</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a href="/experience" className={styles.card}>
             <h2>experience</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
+          <a href="/projects" className={styles.card}>
             <h2>projects</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
+          <a href="/skills" className={styles.card}>
             <h2>skills</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a> */}
+          </a>
         </div>
       </main>
 
