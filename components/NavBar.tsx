b5364770ad3ee844ff7fc069d12eac94cbbd826d
Jordan Hilado <jordanalihilado@gmail.com>
2022-09-10 23:41:47 -0700

    changed format of the navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,7 +6,7 @@ import { Link } from "react-scroll";
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
-  const [mobile] = useMediaQuery("(min-width: 10px)");
+  const [mobile] = useMediaQuery("(min-width: 425px)");
   return (
     <>
       {mobile ? (
@@ -67,48 +67,62 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
         </Box>
       ) : (
         <Box>
-          <Flex justifyContent="center" zIndex={1} position="sticky" p={4}>
-            <Flex
-              width="300px"
-              flexDirection="column"
-              justifyContent="space-between"
-              height="290px"
-            >
+          <Flex
+            zIndex={1}
+            position="fixed"
+            width="100%"
+            // bg="red"
+            top={0}
+            p={4}
+            // bg="rgba(255, 255, 255, 0.2)"
+            // backdropFilter={"blur(10px)"}
+            bg="brand.100"
+            flexDirection="column"
+            alignItems="center"
+          >
+            <Flex width="fit-content" mb={4}>
               <NextLink href="/">
-                <Heading textAlign="center">jordan hilado</Heading>
+                <a>
+                  <Heading size="2xl" color="brand.400">
+                    jordan hilado
+                  </Heading>
+                </a>
               </NextLink>
-              <NextLink href="/education">
-                <Button colorScheme="teal" size="md">
-                  education
-                </Button>
-              </NextLink>
-              <NextLink href="/experience">
-                <Button colorScheme="teal" size="md">
-                  experience
-                </Button>
-              </NextLink>
-              <NextLink href="/projects">
-                <Button colorScheme="teal" size="md">
-                  projects
-                </Button>
-              </NextLink>
-              <NextLink href="/skills">
+            </Flex>
+            <Flex
+              // bg="blue"
+              width="450px"
+              height="100px"
+              flexWrap="wrap"
+              justifyContent="space-around"
+            >
+              <Link to="skills" spy={true} smooth={true} duration={1000}>
                 <Button colorScheme="teal" size="md">
                   skills
                 </Button>
-              </NextLink>
+              </Link>
+              <Link to="education" spy={true} smooth={true} duration={1000}>
+                <Button colorScheme="teal" size="md">
+                  education
+                </Button>
+              </Link>
+              <Link to="experience" spy={true} smooth={true} duration={1000}>
+                <Button colorScheme="teal" size="md">
+                  experience
+                </Button>
+              </Link>
+              <Link to="projects" spy={true} smooth={true} duration={1000}>
+                <Button colorScheme="teal" size="md">
+                  projects
+                </Button>
+              </Link>
               <a
                 href="https://tinyurl.com/hiladojordan2023"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <Button
-                  colorScheme="teal"
-                  width="100%"
-                  size="md"
-                  variant="outline"
-                >
-                  resume
+                <Button colorScheme="teal" size="md" variant="outline">
+                  résumé
                 </Button>
               </a>
             </Flex>
--- components/Profile.tsx
+++ components/Profile.tsx
@@ -55,10 +55,6 @@ export default function Profile() {
             {
               "hi i'm jordan. i have a passion for building full-stack applications. some of my hobbies include listening to music or podcasts, 3d printing, and playing sports like basketball and golf."
             }
-            {/* <Link href={"#"} color={"blue.400"}>
-              #tag
-            </Link> */}
-            {/* me in your posts */}
           </Text>
           <Text
             textAlign={"center"}
@@ -68,33 +64,6 @@ export default function Profile() {
           >
             jordanalihilado@gmail.com
           </Text>
-          {/* <Stack align={"center"} justify={"center"} direction={"row"} mt={6}>
-            <Badge
-              px={2}
-              py={1}
-              bg={useColorModeValue("gray.50", "gray.800")}
-              fontWeight={"400"}
-            >
-              #art
-            </Badge>
-            <Badge
-              px={2}
-              py={1}
-              bg={useColorModeValue("gray.50", "gray.800")}
-              fontWeight={"400"}
-            >
-              #photography
-            </Badge>
-            <Badge
-              px={2}
-              py={1}
-              bg={useColorModeValue("gray.50", "gray.800")}
-              fontWeight={"400"}
-            >
-              #music
-            </Badge>
-          </Stack> */}
-
           <Stack
             width={"100%"}
             mt={"2rem"}
@@ -159,24 +128,6 @@ export default function Profile() {
                 icon={<AiFillTwitterCircle />}
               />
             </Link>
-            {/* <Button
-              flex={1}
-              fontSize={"sm"}
-              rounded={"full"}
-              bg={"blue.400"}
-              color={"white"}
-              boxShadow={
-                "0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
-              }
-              _hover={{
-                bg: "blue.500",
-              }}
-              _focus={{
-                bg: "blue.500",
-              }}
-            >
-              Follow
-            </Button> */}
           </Stack>
         </Stack>
       </Stack>
