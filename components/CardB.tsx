a0fd7b83a0a23d1b7d6179c4fc8f6c0646067240
Jordan Hilado <jordanalihilado@gmail.com>
2023-02-21 23:39:56 -0800

    update button

--- components/CardB.tsx
+++ components/CardB.tsx
@@ -63,23 +63,23 @@ export const CardB = ({
           </Heading>
           <Text color={"brand.400"}>{description}</Text>
         </Stack>
-        <Stack mt={6} direction={"row"} spacing={4} align={"center"}>
+        <Stack mt={3} direction={"row"} spacing={4} align={"center"}>
           {/* <Avatar
             src={"https://avatars0.githubusercontent.com/u/1164541?v=4"}
             alt={"Author"}
           /> */}
           <Stack direction={"column"} spacing={0} fontSize={"sm"}>
-            {link && (
-              <a href={link} target="_blank" rel="noopener noreferrer">
-                <Button mb={3} colorScheme="teal">
-                  View
-                </Button>
-              </a>
-            )}
             <Text color={"brand.500"} fontWeight={600}>
               {position}
             </Text>
             <Text color={"brand.500"}>{date}</Text>
+            {link && (
+              <a href={link} target="_blank" rel="noopener noreferrer">
+                <Button mt={3} colorScheme="teal">
+                  View
+                </Button>
+              </a>
+            )}
           </Stack>
         </Stack>
       </Box>
